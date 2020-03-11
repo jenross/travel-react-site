f236d853973cae7dcbedeb073e188aedc5a64aaf
jenross <ross.m.jennifer@gmail.com>
2020-03-11 14:03:37 -0400

    updated largecard and tours components to pass props

--- src/components/LargeCard.js
+++ src/components/LargeCard.js
@@ -1,60 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function LargeCard({
-  pic,
-  span,
-  heading,
-  details_days,
-  details_people,
-  details_guides,
-  details_lodging,
-  details_difficulty,
-  price
-}) {
-
+export default function LargeCard({ num, heading, description, price }) {
   return (
     <div className="col-1-of-3">
-    <div className="card">
-      <div className="card__side card__side--front">
-        <div className="card__picture card__picture--{pic}">&nbsp;</div>
-        <h4 className="card__heading">
-          <span className="card__heading-span card__heading-span--{span}">
-            {heading}
-          </span>
-        </h4>
-        <div className="card__details">
-          <ul>
-            <li>{details_days}</li>
-            <li>{details_people}</li>
-            <li>{details_guides}</li>
-            <li>{details_lodging}</li>
-            <li>{details_difficulty}</li>
-          </ul>
-        </div>
-      </div>
-      <div className="card__side card__side--back card__side--back-1">
-        <div className="card__cta">
-          <div className="card__price-box">
-            <p className="card__price-only">Only</p>
-            <p className="card__price-value">{price}</p>
+      <div className="card">
+        <div className="card__side card__side--front">
+          <div className={`card__picture card__picture--${num}`}>&nbsp;</div>
+          <h4 className="card__heading">
+            <span className={`card__heading-span card__heading-span--${num}`}>
+              {heading}
+            </span>
+          </h4>
+          <div className="card__details">
+            <ul>
+              {description &&
+                description.map((desc, index) => {
+                  return <li key={index}>{desc}</li>;
+                })}
+            </ul>
           </div>
-          <a href="#popup" className="btn btn--white">
-            Book now!
-          </a>
+        </div>
+        <div className={`card__side card__side--back card__side--back-${num}`}>
+          <div className="card__cta">
+            <div className="card__price-box">
+              <p className="card__price-only">Only</p>
+              <p className="card__price-value">{price}</p>
+            </div>
+            <a href="#popup" className="btn btn--white">
+              Book now!
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
 LargeCard.propTypes = {
+  num: PropTypes.number.isRequired,
   heading: PropTypes.string.isRequired,
-  details_days: PropTypes.string.isRequired,
-  details_people: PropTypes.string.isRequired,
-  details_guides: PropTypes.string.isRequired,
-  details_lodging: PropTypes.string.isRequired, 
-  details_difficulty: PropTypes.string.isRequired, 
+  description: PropTypes.array.isRequired,
   price: PropTypes.string.isRequired
 };
--- src/components/Tours.js
+++ src/components/Tours.js
@@ -2,36 +2,6 @@ import React from "react";
 import LargeCard from "./LargeCard";
 
 const Tours = () => {
-  const tourDetails = [
-    {
-      sea_pic: "1",
-      sea_heading: "The Sea Explorer",
-      sea_days: "3 day tours",
-      sea_people: "Up to 30 people",
-      sea_guides: "2 tour guides",
-      sea_lodging: "Sleep in cozy hotels",
-      sea_difficulty: "Difficulty: easy",
-      sea_price: "$297"
-    },
-    {
-      forest_heading: "The Forest Hiker",
-      forest_days: "7 day tours",
-      forest_people: "Up to 40 people",
-      forest_guides: "6 tour guides",
-      forest_lodging: "Sleep in provided tents",
-      forest_difficulty: "Difficulty: medium",
-      forest_price: "$497"
-    },
-    {
-      sea_heading: "The Sea Explorer",
-      sea_days: "3 day tours",
-      sea_people: "Up to 30 people",
-      sea_guides: "2 tour guides",
-      sea_lodging: "Sleep in cozy hotels",
-      sea_difficulty: "Difficulty: easy",
-      sea_price: "$297"
-    }
-  ];
   return (
     <section className="section-tours" id="section-tours">
       <div className="u-center-text u-margin-bottom-big">
@@ -40,58 +10,43 @@ const Tours = () => {
 
       <div className="row">
         <LargeCard
-          pic={tourDetails[0].sea_pic}
-          heading={tourDetails[0].sea_heading}
-          details_days={tourDetails[0].sea_days}
-          details_people={tourDetails[0].sea_people}
-          details_guides={tourDetails[0].sea_guides}
-          details_lodging={tourDetails[0].sea_lodging}
-          details_difficulty={tourDetails[0].sea_difficulty}
-          price={tourDetails[0].sea_price}
+          num="1"
+          heading="The Sea Explorer"
+          description={[
+            "3 day tours",
+            "Up to 30 people",
+            "2 tour guides",
+            "Sleep in cozy hotels",
+            "Difficulty: easy"
+          ]}
+          price="$297"
         />
 
         <LargeCard
-          heading={tourDetails[1].forest_heading}
-          details_days={tourDetails[1].forest_days}
-          details_people={tourDetails[1].forest_people}
-          details_guides={tourDetails[1].forest_guides}
-          details_lodging={tourDetails[1].forest_lodging}
-          details_difficulty={tourDetails[1].forest_difficulty}
-          price={tourDetails[1].forest_price}
+          num="2"
+          heading="The Forest Hiker"
+          description={[
+            "7 day tours",
+            "Up to 40 people",
+            "5 tour guides",
+            "Sleep in provided tents",
+            "Difficulty: medium"
+          ]}
+          price="$497"
         />
 
-        <div className="col-1-of-3">
-          <div className="card">
-            <div className="card__side card__side--front">
-              <div className="card__picture card__picture--3">&nbsp;</div>
-              <h4 className="card__heading">
-                <span className="card__heading-span card__heading-span--3">
-                  The Snow Adventurer
-                </span>
-              </h4>
-              <div className="card__details">
-                <ul>
-                  <li>5 day tours</li>
-                  <li>Up to 15 people</li>
-                  <li>3 tour guides</li>
-                  <li>Sleep in provided tents</li>
-                  <li>Difficulty: hard</li>
-                </ul>
-              </div>
-            </div>
-            <div className="card__side card__side--back card__side--back-3">
-              <div className="card__cta">
-                <div className="card__price-box">
-                  <p className="card__price-only">Only</p>
-                  <p className="card__price-value">$897</p>
-                </div>
-                <a href="#popup" className="btn btn--white">
-                  Book now!
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <LargeCard
+          num="3"
+          heading="The Snow Adventurer"
+          description={[
+            "5 day tours",
+            "Up to 15 people",
+            "3 tour guides",
+            "Sleep in igloos",
+            "Difficulty: hard"
+          ]}
+          price="$897"
+        />
       </div>
 
       <div className="u-center-text u-margin-top-huge">
