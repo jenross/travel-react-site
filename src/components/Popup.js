7d3659287dd0009d719f0795bbb4dc03b035f417
jenross <ross.m.jennifer@gmail.com>
2020-04-12 08:19:04 -0400

    updated readme and meta description; changed popup text

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -14,7 +14,6 @@ const Popup = () => {
           <img src={nat9} alt="Tour photo" className="popup__img" />
         </div>
         <div className="popup__right">
-     
           <a href="#section-tours" className="popup__close">
             &times;
           </a>
@@ -22,20 +21,18 @@ const Popup = () => {
 
           <HeadingTertiary text="Important &ndash; Please read these terms before booking" />
           <p className="popup__text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed sed
-            risus pretium quam. Aliquam sem et tortor consequat id. Volutpat
-            odio facilisis mauris sit amet massa vitae. Mi bibendum neque
-            egestas congue. Placerat orci nulla pellentesque dignissim enim sit.
-            Vitae semper quis lectus nulla at volutpat diam ut venenatis.
-            Malesuada pellentesque elit eget gravida cum sociis natoque
-            penatibus et. Proin fermentum leo vel orci porta non pulvinar neque
-            laoreet. Gravida neque convallis a cras semper. Molestie at
-            elementum eu facilisis sed odio morbi quis. Faucibus vitae aliquet
-            nec ullamcorper sit amet risus nullam eget. Nam libero justo laoreet
-            sit. Amet massa vitae tortor condimentum lacinia quis vel eros
-            donec. Sit amet facilisis magna etiam. Imperdiet sed euismod nisi
-            porta.
+            I'm baby trust fund kogi gluten-free everyday carry lomo narwhal
+            occupy. Poutine vaporware everyday carry artisan shoreditch fixie.
+            Seitan yr leggings etsy literally tbh. Tbh scenester etsy, hot
+            chicken iPhone tote bag edison bulb seitan. Copper mug edison bulb
+            wayfarers polaroid chartreuse salvia pickled DIY cold-pressed
+            distillery woke taxidermy. Four dollar toast hella you probably
+            haven't heard of them +1. Glossier stumptown cronut, chia master
+            cleanse kale chips bicycle rights yuccie vegan retro pabst
+            williamsburg lo-fi forage. 90's mixtape microdosing jianbing. Edison
+            bulb skateboard deep v brooklyn wayfarers pabst. Tumblr chambray
+            flexitarian copper mug sriracha, ugh blue bottle taiyaki adaptogen
+            snackwave edison bulb cold-pressed vape kale chips vaporware.
           </p>
           <Button link="#" color="green" text="Book now" />
         </div>
